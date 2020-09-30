7daa168cff4ddef769cc3cd394a6423653e364ce
Nina-Mathew <71400436+Nina-Mathew@users.noreply.github.com>
2020-09-30 12:56:34 -0400

    traffic light red green yellow

--- main.ts
+++ main.ts
@@ -1,6 +1,17 @@
 // Created by: Nina Mathew
 // Created on: 9/30/20
 // This program lights up Neopixels
+let strip = neopixel.create(DigitalPin.P16, 4, NeoPixelMode.RGB)
+strip.show()
+basic.pause(1000)
+strip.setPixelColor(3, neopixel.colors(NeoPixelColors.Red))
+strip.show()
+basic.pause(1000)
+strip.setPixelColor(2, neopixel.colors(NeoPixelColors.Yellow))
+strip.show()
+basic.pause(1000)
+strip.setPixelColor(1, neopixel.colors(NeoPixelColors.Green))
+strip.show()
 basic.forever(function () {
 	
 })
